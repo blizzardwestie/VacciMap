a5502ca0966104cf96a7de9cf415d0bf6384e1a8
test <test>
2021-01-29 16:23:13 -0600

    Initial Commit

---
+++ VacciMap/VacciMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacciMapApp.swift
+//  VacciMap
+//
+//  Created by Matthew Chertok on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct VacciMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
